521609e357f7db27dc935b437d5cb06a2d274f54
ahegde5@uic.edu <ahegde5@uic.edu>
2018-04-28 17:38:06 -0500

    added logging dependency

---
+++ src/main/java/com/uic/atse/Main.java
@@ -0,0 +1,14 @@
+package com.uic.atse;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    static Logger logger = Logger.getLogger(Main.class.getName());
+    public static void main(String[] args){
+
+        logger.info("Welcome to the project");
+    }
+
+
+}
